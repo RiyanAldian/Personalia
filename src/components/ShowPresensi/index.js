e5f454935ce67189354da7b5e8efade238235b05
RiyanAldian <riyanaldiansyah37@gmail.com>
2022-11-22 12:27:27 +0700

    d

--- src/components/ShowPresensi/index.js
+++ src/components/ShowPresensi/index.js
@@ -6,7 +6,7 @@ export default function ShowPresensi() {
   return (
     <View style={styles.container}>
         <View style={styles.row}>
-            <Text>RR</Text>
+            <Text>RRddd</Text>
         </View>
     </View>
   );
